683a9f34b999c9f963bfedb330fe9ec4bafe7445
a-asil-companioni <aasil@iom.int>
2025-07-30 09:26:56 +0100

    Use withr for set_subscription_key test withr is an R library that allows changes to state to be reverted and managed cleanly.
    Add withr package to DESCRIPTION file

--- tests/testthat/test-set_subscription_key.R
+++ tests/testthat/test-set_subscription_key.R
@@ -1,15 +1,8 @@
-test_that("DTM API subscription key set as environmental variable.", {
+test_that("DTM API subscription key environment variable is changed.", {
   skip_on_cran()
 
-  env_var_before_call <- Sys.getenv()["DTM_SUBSCRIPTION_KEY"] |> as.vector()
-
+  withr::local_envvar(DTM_SUBSCRIPTION_KEY = Sys.getenv("DTM_SUBSCRIPTION_KEY"))
   subscription_key_input <- "dummyValue"
   set_subscription_key(key = subscription_key_input)
-  
-  # as.vector() is used to remove all attributes including names.
-  env_var_after_call <- Sys.getenv()["DTM_SUBSCRIPTION_KEY"] |> as.vector()
-  # Restore old env variable value to what it was before the function was tested.
-  Sys.setenv("DTM_SUBSCRIPTION_KEY" = env_var_before_call)
-
-  expect_true(env_var_after_call == subscription_key_input)
+  expect_equal(Sys.getenv("DTM_SUBSCRIPTION_KEY"), subscription_key_input)
 })
